1eb87d7c905377c7ed269e07f18b55f6cd5bba66
nkshn <vovanika23413@gmail.com>
2019-12-28 02:31:18 +0200

    Added Image to 'GameOverScreen' screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,17 +1,20 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, StyleSheet, Button, Image } from "react-native";
 
-import Colors from "../constants/colors";
+import Colors from '../constants/colors';
+import SubTitle from '../components/SubTitle';
+import MainTitle from '../components/MainTitle';
 
 const GameOverScreen = props => {
   const { counterTries, userSelectedDigit, restartGame } = props;
 
   return (
     <View style={styles.screen}>
-      <Text>Game is Over!</Text>
-      <Text>Tries: {counterTries}</Text>
-      <Text>User Digit: {userSelectedDigit}</Text>
-      <Button title="New Game" onPress={restartGame} />
+      <MainTitle>Game is Over!</MainTitle>
+      <View style={styles.imageContainer}><Image source={require('../assets/images/success.png')} style={styles.image} resizeMode="cover" /></View>
+      <SubTitle>User Digit: {userSelectedDigit}</SubTitle>
+      <SubTitle>Tries: {counterTries}</SubTitle>
+      <View style={{ width: '35%', marginTop: 10 }}><Button title="New Game" onPress={restartGame} color={Colors.mainGreen} /></View>
     </View>
   );
 };
@@ -21,7 +24,20 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
-  }
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.mainPink,
+    overflow: 'hidden',
+    marginVertical: 15,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
 });
 
 export default GameOverScreen;
